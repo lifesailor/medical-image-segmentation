5e54bd47ec3f3bb988512587c8ab09ff75bf760e
lifesailor <sailyourlife91@gmail.com>
2019-01-29 23:25:51 +0900

    change image size

--- utils/parser.py
+++ utils/parser.py
@@ -14,7 +14,7 @@ def parse_args():
     parser.add_argument("-IS", "--image_size", default=(256, 256, 3), help="input image size.")
     parser.add_argument("-BS", "--batch_size", required=False, default=32, help="Initial learning rate.")
     parser.add_argument("-E", "--epochs", required=False, default=30, help="How much to decay the learning rate.")
-    parser.add_argument("-LI", "--lr_init", required=False, default=1e-4, help="Initial learning rate.")
+    parser.add_argument("-LI", "--lr_init", required=False, default=1e-3, help="Initial learning rate.")
     parser.add_argument("-LD", "--lr_decay", required=False, default=5e-4, help="How much to decay the learning rate.")
     args = parser.parse_args()
     return args
